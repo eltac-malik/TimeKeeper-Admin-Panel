fb4dd9c74dea939b79e719b2c4460ab2dd36a3ae
Eltac E. Malikmammadov <Eltacem@code.local>
2022-08-14 19:49:10 +0400

    com-3

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import '../assets/css/Login.css'
+import {Formik,Form,Field} from 'formik'
+import axios from 'axios';
 
 
 function Login() {
     return (
         <div className='login'>
-            Login
+            
+            <div className="lg-form">
+                <Formik
+                initialValues={{
+                    username:""
+                }}
+                onSubmit={(x)=>
+                {
+                    console.log(x)
+                }}
+                >
+                    <Form className='form'>
+                        <div className="lg-l">
+                        <label htmlFor="usr">Username</label>
+                        <Field name='username' id='usr'/>
+                        </div>
+                        <div className="lg-l">
+                        <label htmlFor="usr">Password</label>
+                        <Field name='password' type='password' id='usr'/>
+                        </div>
+                        <input type="submit" value='Submit'/>
+                    </Form>
+                </Formik>
+            </div>
+
         </div>
     )
 }
